9d0614595a65d9900eb09d21c73a8e019ec584ed
zeerodex <sdemianenko25@gmail.com>
2025-09-10 10:43:05 +0300

    2025-09-10

--- .config/hypr/scripts/start-waybar.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Give services some time to start up
-sleep 2
-
-# Check if Waybar is already running (optional, but good practice)
-if ! pgrep -x "waybar" > /dev/null; then
-    waybar &
-fi
